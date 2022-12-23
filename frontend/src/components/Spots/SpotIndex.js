af5b5d895b0cfd880672304e373349cd53e3c274
andrew <a.kimswk@gmail.com>
2022-12-22 16:29:25 -0800

    added dev links

--- frontend/src/components/Spots/SpotIndex.js
+++ frontend/src/components/Spots/SpotIndex.js
@@ -21,49 +21,61 @@ const SpotIndex = () => {
   //   console.log('LOOKHERE', spot)
   // ))
 
-  if (!spots) {
-    return null
-  } else {
-    return (
+  // if (!spots) {
+  //   return null
+  // } else {
+  return (
+    <div>
+      <div id="dev-icons">
+        <span id="dev-button-container">
+          <a href='https://github.com/k-rewd' target='_blank' rel='noreferrer'>
+          <i class="fa-brands fa-github"></i></a>
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-angellist"></i>
+          <i class="fa-brands fa-dev"></i>
+        </span>
+
+      </div>
       <div id="outer-most-spots">
         <div className="frame">
           {Object.values(spots).map(spot => (
             <div className="single-card" key={spot.id}>
-            <NavLink to={`/spots/${spot.id}`} >
-              <div className="spot-card-flex-column">
-                <div id='single-card-img-container'>
-                  <img onError={(e)=> e.target.src="https://cdn-icons-png.flaticon.com/512/70/70644.png"}className="spot-image" src={spot.previewImage} alt='img' />
-                </div>
-                <div className="cityState-avgRating-flex-row">
-                  <div className="city-state">
-                    {spot.city}, {spot.state}, {spot.country}
+              <NavLink to={`/spots/${spot.id}`} >
+                <div className="spot-card-flex-column">
+                  <div id='single-card-img-container'>
+                    <img onError={(e) => e.target.src = "https://cdn-icons-png.flaticon.com/512/70/70644.png"} className="spot-image" src={spot.previewImage} alt='img' />
+                  </div>
+                  <div className="cityState-avgRating-flex-row">
+                    <div className="city-state">
+                      {spot.city}, {spot.state}, {spot.country}
+                    </div>
+                    <div>
+                      ★{spot.avgRating}
+                    </div>
                   </div>
                   <div>
-                  ★{spot.avgRating}
+                    {spot.name}
+                  </div>
+                  <div>
+                    {spot.address}
+                  </div>
+                  <div className="spot-price-night">
+                    <div id="spot-price">
+                      $ {spot.price}
+                    </div>
+                    <div id="spot-night">
+                      night
+                    </div>
                   </div>
                 </div>
-                <div>
-                  {spot.name}
-                </div>
-                <div>
-                  {spot.address}
-                </div>
-                <div className="spot-price-night">
-                  <div id="spot-price">
-                    $ {spot.price}
-                  </div>
-                  <div id="spot-night">
-                    night
-                  </div>
-                </div>
-              </div>
-            </NavLink>
-          </div>
+              </NavLink>
+            </div>
           ))}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+  // }
 }
 
 export default SpotIndex;
