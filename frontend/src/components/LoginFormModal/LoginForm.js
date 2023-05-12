ec9f587caff1c31cc3a8bc49cc0eb2fe950a3e90
andrew <a.kimswk@gmail.com>
2023-05-12 01:49:29 -0700

    modify sign in

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -31,37 +31,37 @@ function LoginForm() {
   return (
     <div className='login-form-modal'>
       <h2 className="welcome-message">Welcome to the EBDB BnB</h2>
-      <form onSubmit={handleSubmit}>
-        <div className='user-input-action-button'>
+      <form onSubmit={handleSubmit} id='login-form'>
 
-            {/* <label className='input-prompt-label'> Username or Email </label> */}
-            <input className='login-username-email'
-              type="text"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              placeholder="Username or Email"
-              />
 
-            {/* <label className='input-prompt-label'> Password</label> */}
-            <input className='login-password'
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-               />
-          
-          <div className='two-buttons-row'>
-            <div><button className='pink-buttons' type="submit">Log In</button></div>
-            <div><button className='pink-buttons' onClick={() => {
-              setCredential("Childish")
-              setPassword("password")
-            }}>DEMO USER</button>
-            </div>
+        {/* <label className='input-prompt-label'> Username or Email </label> */}
+        <input className='login-username-email'
+          type="text"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          placeholder="Username or Email"
+        />
+
+        {/* <label className='input-prompt-label'> Password</label> */}
+        <input className='login-password'
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+
+        <div className='two-buttons-row'>
+          <div><button className='pink-buttons' type="submit">Log In</button></div>
+          <div><button className='pink-buttons' onClick={() => {
+            setCredential("Childish")
+            setPassword("password")
+          }}>DEMO USER</button>
           </div>
         </div>
 
 
 
+      </form>
         <div className="error-message-container">
           <ul>
             {errors.map((error, idx) => (
@@ -69,7 +69,6 @@ function LoginForm() {
             ))}
           </ul>
         </div>
-      </form>
     </div>
   );
 }
