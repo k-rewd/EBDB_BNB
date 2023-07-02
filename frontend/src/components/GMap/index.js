d5de9c8faa8c69cbca4703599861cabcfb0e75b4
andrew <a.kimswk@gmail.com>
2023-07-01 21:40:11 -0700

    asdf

--- frontend/src/components/GMap/index.js
+++ frontend/src/components/GMap/index.js
@@ -2,3 +2,5 @@
 //   GoogleMapProvider,
 //   useGoogleMap,
 // } from '@ubilabs/google-maps-react-hooks'
+
+// https://www.youtube.com/watch?v=s4n_x5B58Dw&list=PL2rFahu9sLJ2QuJaKKYDaJp0YqjFCDCtN&index=3
