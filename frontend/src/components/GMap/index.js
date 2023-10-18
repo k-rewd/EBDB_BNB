5140fcba753c0310d415222982f76d3451725f01
andrew <a.kimswk@gmail.com>
2023-10-17 18:12:41 -0700

    adding map component

--- frontend/src/components/GMap/index.js
+++
@@ -1,6 +0,0 @@
-// import {
-//   GoogleMapProvider,
-//   useGoogleMap,
-// } from '@ubilabs/google-maps-react-hooks'
-
-// https://www.youtube.com/watch?v=s4n_x5B58Dw&list=PL2rFahu9sLJ2QuJaKKYDaJp0YqjFCDCtN&index=3
---
+++ frontend/src/components/Map/Map.js
@@ -0,0 +1,14 @@
+// import {
+//   GoogleMapProvider,
+//   useGoogleMap,
+// } from '@ubilabs/google-maps-react-hooks'
+
+// https://www.youtube.com/watch?v=s4n_x5B58Dw&list=PL2rFahu9sLJ2QuJaKKYDaJp0YqjFCDCtN&index=3
+
+function Map() {
+  return (
+    <div></div>
+  )
+}
+
+export default Map
